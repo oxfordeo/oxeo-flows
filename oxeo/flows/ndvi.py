e957458c0fd8d2e7def68d028a0bc1dea7425df1
Chris Arderne <chris@rdrn.me>
2022-07-20 16:30:13 +0100

    NDVI use predictor

--- oxeo/flows/ndvi.py
+++ oxeo/flows/ndvi.py
@@ -1,37 +1,30 @@
 import json
 import os
-from datetime import datetime, timedelta
+from datetime import timedelta
 from typing import List, Optional
 
-import dateparser  # type: ignore
 import prefect
-import pystac_client
+import geopandas as gpd
+import pandas as pd
 import httpx
-import stackstac
 from dask.distributed import LocalCluster
 from dask_kubernetes import KubeCluster, make_pod_spec
-from prefect import Flow, Parameter, task, unmapped
+from prefect import Flow, Parameter, task
 from prefect.client import Secret
 from prefect.executors import DaskExecutor, LocalExecutor
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GitHub
 from prefect.tasks.secrets import PrefectSecret
-from pyproj import CRS, Transformer
-from pystac.item import Item
-from rasterio import features
-from rasterio.enums import Resampling
-from rasterio.errors import RasterioIOError
-from shapely import geometry
-from shapely.ops import transform as transform_proj
+from oxeo.water.models.ndvi import NDVIPredictor
+from sentinelhub import BBox, CRS
 
-from oxeo.flows.models import EventCreate, Feature
+from oxeo.flows.models import EventCreate
+
+Box = tuple[float, float, float, float]
 
 
-@task(log_stdout=True, max_retries=1, retry_delay=timedelta(seconds=10))
-def extract(_id: int, U: Optional[str] = None, P: Optional[str] = None) -> Feature:
-    logger = prefect.context.get("logger")
-    logger.info(f"Extracting AOI {_id}")
-
+@task(log_stdout=True)
+def get_box(aoi_id: int, U: Optional[str] = None, P: Optional[str] = None) -> Box:
     # login
     if not U or not P:
         U = os.environ.get("username")
@@ -46,110 +39,54 @@ def extract(_id: int, U: Optional[str] = None, P: Optional[str] = None) -> Featu
     headers = {"Authorization": f"Bearer {token}"}
 
     print("Get AOI")
-    r = client.get("aoi/", params=dict(id=_id), headers=headers)
+    r = client.get("aoi/", params=dict(id=aoi_id), headers=headers)
     j = json.loads(r.text)
-    ft = Feature(**j["features"][0])
 
     print("Close httpx client")
     client.close()
-
-    return ft
-
-
-@task(log_stdout=True, max_retries=1, retry_delay=timedelta(seconds=10))
-def stac(
-    aoi: Feature,
-    start_datetime: datetime,
-    end_datetime: datetime,
-) -> List[Item]:
-    logger = prefect.context.get("logger")
-
-    URL = "https://earth-search.aws.element84.com/v0"
-    catalog = pystac_client.Client.open(URL)
-
-    start_datetime = dateparser.parse(start_datetime)
-    end_datetime = dateparser.parse(end_datetime)
-    print(start_datetime, end_datetime)
-
-    items = catalog.search(
-        intersects=geometry.mapping(
-            geometry.box(*(geometry.shape(aoi.geometry.__dict__).bounds))
-        ),
-        collections=["sentinel-s2-l2a-cogs"],
-        datetime=f"{start_datetime.isoformat()[0:10]}/{end_datetime.isoformat()[0:10]}",
-        max_items=None,
-        limit=100,
-    ).items()
-
-    items = [i for i in items]
-    logger.info(f"Got {len(items)} STAC items")
-
-    return items
+    gdf = gpd.GeoDataFrame.from_features(j)
+    box: Box = tuple(gdf.total_bounds)  # type: ignore[assignment]
+    print(box)
+    return box
 
 
 @task(log_stdout=True, max_retries=1, retry_delay=timedelta(seconds=10))
-def transform(aoi: Feature, item: Item) -> EventCreate:
+def transform(
+    aoi_id: int, box: Box, start_datetime: str, end_datetime: str
+) -> list[EventCreate]:
     logger = prefect.context.get("logger")
     logger.info("NDVI transforming.")
 
-    geom = geometry.shape(aoi.geometry.__dict__)
+    bbox = BBox(box, crs=CRS.WGS84)
 
-    xr_stack = stackstac.stack(
-        item,
-        resolution=10,
-        bounds_latlon=geom.bounds,
-        resampling=Resampling.bilinear,
-        errors_as_nodata=(
-            IOError(r"HTTP response code: \d\d\d"),
-            RasterioIOError(".*"),
-        ),
+    predictor = NDVIPredictor()
+    URL = "https://earth-search.aws.element84.com/v0"
+    ndvi = predictor.predict_stac_aoi(
+        catalog=URL,
+        data_collection="sentinel-s2-l2a-cogs",
+        bbox=bbox,
+        time_interval=(start_datetime, end_datetime),
+        search_params={"max_items": None},
     )
-
-    # swap geom to utm
-    crs_wgs84 = CRS("EPSG:4326")
-    crs_utm = CRS(xr_stack.crs)
-    wgs2utm = Transformer.from_crs(crs_wgs84, crs_utm, always_xy=True).transform
-    utm_geom = transform_proj(wgs2utm, geom)
-
-    out_shp = (xr_stack.coords["y"].shape[0], xr_stack.coords["x"].shape[0])
-
-    # burn in mask
-    mask_arr = features.rasterize(
-        [utm_geom],
-        out_shape=out_shp,
-        fill=0,
-        transform=xr_stack.transform,
-        all_touched=False,
-        default_value=1,
-        dtype=None,
-    )
-
-    # build computation graph for NDVI: (NIR-red) / (NIR+RED)
-    xr_stack.coords["mask"] = (("y", "x"), mask_arr)
-
-    xr_stack = xr_stack.where(xr_stack.mask == 1)
-
-    xr_ndvi = (xr_stack.sel({"band": "B08"}) - xr_stack.sel({"band": "B04"})) / (
-        xr_stack.sel({"band": "B08"}) + xr_stack.sel({"band": "B04"})
-    )
-    xr_ndvi = xr_ndvi.mean(dim=["x", "y"])
 
     # call the compute with the dask backend
-    result = xr_ndvi.compute()
+    ndvi_red = ndvi.mean(dim=["x", "y"])
+    res = ndvi_red.compute()
 
-    # cast to pandas
-    df = result.to_pandas()
-    df = df.fillna(0)
-    df.index = df.index.date
+    ndvi_ts = res.data
+    dates = ndvi_red.time.data
 
-    event = EventCreate(
-        labels="ndvi",
-        aoi_id=aoi.id,
-        datetime=df.index[0],
-        keyed_values={"mean_value": df[0]},
-    )
+    events = [
+        EventCreate(
+            labels="ndvi",
+            aoi_id=aoi_id,
+            datetime=pd.Timestamp(d).date(),
+            keyed_values={"mean_value": int(w)},
+        )
+        for d, w in zip(dates, ndvi_ts)
+    ]
 
-    return event
+    return events
 
 
 @task(log_stdout=True)
@@ -261,9 +198,8 @@ def create_flow():
         start_datetime = Parameter(name="start_datetime", default="2020-01-01")
         end_datetime = Parameter(name="end_datetime", default="2020-01-08")
 
-        aoi = extract(aoi_id, api_username, api_password)
-        items = stac(aoi, start_datetime, end_datetime)
-        events = transform.map(unmapped(aoi), items)
+        box = get_box(aoi_id, api_username, api_password)
+        events = transform(aoi_id, box, start_datetime, end_datetime)
         _ = load(events)
 
     return flow
