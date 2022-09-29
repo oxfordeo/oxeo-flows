ca132acb22bac4279d72aa603e482a84e68f1c02
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2022-09-29 18:06:48 +0000

    abstract water flow to multiconst

--- oxeo/flows/water.py
+++ oxeo/flows/water.py
@@ -1,13 +1,13 @@
 import json
 import os
-from typing import List, Optional, Union
+from typing import Optional, Union
 
 import geopandas as gpd
-import pandas as pd
 import httpx
+import pandas as pd
+import prefect
 import s3fs
 from dask_kubernetes import KubeCluster, make_pod_spec
-from distributed import Client
 from prefect import Flow, Parameter, task
 from prefect.client import Secret
 from prefect.executors import LocalExecutor
@@ -16,9 +16,7 @@ from prefect.storage import GitHub
 from prefect.tasks.secrets import PrefectSecret
 from sentinelhub import CRS, BBox, SHConfig
 
-from oxeo.core.constants import BAND_PREDICTOR_ORDER
-from oxeo.core.stac.constants import ELEMENT84_URL, USWEST_URL
-from oxeo.flows.models import EventCreate
+from oxeo.core.stac.constants import ELEMENT84_URL, LANDSATLOOK_URL, USWEST_URL
 
 Box = tuple[float, float, float, float]
 
@@ -80,14 +78,22 @@ def predict(
     box: Box,
     start_date,
     end_date,
+    constellation,
     cluster: Optional[KubeCluster] = None,
-) -> list[EventCreate]:
+) -> list[dict]:
     from oxeo.water.models.segmentation import (
         DaskSegmentationPredictor,
         reconstruct_image_from_patches,
         reduce_to_timeseries,
         stack_preds,
     )
+
+    assert constellation in [
+        "sentinel-2",
+        "landsat-5",
+        "landsat-7",
+        "landsat-8",
+    ], """constalltion must be one of ['sentinel-2','landsat-5','landsat-7','landsat-8']"""
 
     shconfig = SHConfig()
     shconfig.sh_base_url = USWEST_URL
@@ -101,73 +107,95 @@ def predict(
 
     bbox = BBox(box, crs=CRS.WGS84)
 
-    print("Connect client")
-    if cluster:
-        client = Client(cluster)
-    else:
-        client = Client(n_workers=8, threads_per_worker=1, memory_limit="64GB")
+    # print("Connect client")
+    # if cluster:
+    #     client = Client(cluster)
+    # else:
+    #     client = Client(n_workers=8, threads_per_worker=1, memory_limit="64GB")
+
+    if "sentinel" in constellation.lower():
+        URL = ELEMENT84_URL
+        collection = "sentinel-s2-l2a-cogs"
+        search_params = {
+            "query": {
+                "eo:cloud_cover": {"gte": 0, "lte": 10},
+            }
+        }
+        const_str = "sentinel-2"
+    elif "landsat" in constellation.lower():
+        URL = LANDSATLOOK_URL
+        collection = "landsat-c2l2-sr"
+        search_params = {
+            "query": {
+                "platform": {"in": [constellation.upper().replace("-", "_")]},
+                "eo:cloud_cover": {"gte": 0, "lte": 10},
+            }
+        }  # noqa
+        const_str = "landsat"
 
     print("Creating graph")
-    s2_predictor = DaskSegmentationPredictor(
+    predictor = DaskSegmentationPredictor(
         ckpt_path=ckpt_path,
         fs=fs,
-        bands=BAND_PREDICTOR_ORDER["sentinel-2"],
     )
-    preds, aoi = s2_predictor.predict_stac_aoi(
-        constellation="sentinel-2",
-        catalog=ELEMENT84_URL,
-        data_collection="sentinel-s2-l2a-cogs",
+    preds, aoi = predictor.predict_stac_aoi(
+        constellation=const_str,
+        catalog=URL,
+        data_collection=collection,
         bbox=bbox,
         time_interval=(start_date, end_date),
-        search_params={},
+        search_params=search_params,
+        resolution=10,
     )
 
     stack = stack_preds(preds)
     revisits, _, target_h, target_w = aoi.shape
-    mask = reconstruct_image_from_patches(stack, revisits, target_h, target_w, 250)
+    mask = reconstruct_image_from_patches(
+        stack, revisits, target_h, target_w, patch_size=250
+    )
     ts = reduce_to_timeseries(mask)
-
-    print("Compute results")
-    res = client.compute(ts)
-    water_ts = res.result().compute()
-
-    print("Close cluster (if present)")
-    if cluster:
-        cluster.close()
+    dates = aoi.time.data
 
     print("Create Events")
     dates = aoi.time.data
     events = [
-        EventCreate(
-            labels="water_extents",
+        dict(
+            labels=["water_extents"],
             aoi_id=aoi_id,
             datetime=pd.Timestamp(d).date(),
             keyed_values={"water_pixels": int(w)},
         )
-        for d, w in zip(dates, water_ts)
+        for d, w in zip(dates, ts)
     ]
 
     return events
 
 
 @task(log_stdout=True)
-def load(events: List[EventCreate]) -> bool:
+def load(events: list[dict], U: Optional[str] = None, P: Optional[str] = None) -> bool:
     print("Loading events into DB")
 
-    for secret in ["PG_DB_USER", "PG_DB_PW", "PG_DB_HOST", "PG_DB_NAME"]:
-        os.environ[secret] = str(Secret(secret).get())
+    logger = prefect.context.get("logger")
 
-    # TODO
-    # These must only be imported here, as api.models.database
-    # loads the env vars in global scope and creates the DB URL
-    from oxeo.api.controllers.geom import create_events
-    from oxeo.api.models.database import SessionLocal
+    # refresh headers
+    if not U or not P:
+        U = os.environ.get("username")
+        P = os.environ.get("password")
 
-    db = SessionLocal()
-    create_events(events, db, None)
-    db.close()
+    base_url = "https://api.oxfordeo.com/"
+    client = httpx.Client(base_url=base_url)
 
-    print(f"Successfully inserted {len(events)=} events into the db")
+    r = client.post("auth/token", data={"username": U, "password": P})
+    token = json.loads(r.text)["access_token"]
+    headers = {"Authorization": f"Bearer {token}"}
+
+    r = client.post("events/", json=events, headers=headers)
+    if str(r.status_code) != "201":
+        raise ValueError(f"Status code: {r.status_code}")
+
+    client.close()
+
+    logger.info(f"Successfully inserted {len(events)} events into the db")
 
     return True
 
@@ -197,13 +225,14 @@ def create_flow():
         api_username = "admin@oxfordeo.com"
         api_password = PrefectSecret("API_PASSWORD")
         aoi_id = Parameter(name="aoi_id", default=2010)
+        constellation = Parameter(name="constellation", default="sentinel-2")
         start_date = Parameter(name="start_date", default="2020-12-10")
         end_date = Parameter(name="end_date", default="2021-02-01")
 
         box = get_box(aoi_id, api_username, api_password)
         cluster = create_cluster(n_workers, cpu, memory)
-        events = predict(aoi_id, box, start_date, end_date, cluster)
-        _ = load(events)
+        events = predict(aoi_id, box, start_date, end_date, constellation, cluster)
+        _ = load(events, api_username, api_password)
 
     return flow
 
